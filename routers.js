8d7f7fe44dff6591f232ed63a6dbd61367a7a575
Alberto Silva <albertossilva@gmail.com>
2016-06-15 16:15:51 -0300

    When its localhost add the project id as basePath

--- routers.js
+++ routers.js
@@ -3,10 +3,17 @@ import { Router, Route, Link, browserHistory } from 'react-router';
 
 import MyContainerContainer from './MyContainer/MyContainerContainer';
 
+let baseUrl = '/';
+const { hostname, pathname } = window.location;
+
+if(hostname === 'localhost') {
+  baseUrl = pathname.match(/^\/project\/([a-f0-9]+\-)+[a-f0-9]+\/?/)[0];
+}
+
 export default (App) => {
     return (
         <Router history={browserHistory}>
-            <Route path="/" component={App}>
+            <Route path={baseUrl} component={App}>
                 <Route path="mycontainer" component={MyContainerContainer} />
             </Route>
         </Router>
