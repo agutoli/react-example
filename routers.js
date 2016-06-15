35d11efccd93494c3834009d026d3fd4d2a51238
Alberto Silva <albertossilva@gmail.com>
2016-06-15 17:52:17 -0300

    Making it work with other base urls

--- routers.js
+++ routers.js
@@ -4,12 +4,11 @@ import { Router, Route, Link, browserHistory } from 'react-router';
 import MyContainerContainer from './MyContainer/MyContainerContainer';
 
 let baseUrl = '/';
-const { hostname, pathname } = window.location;
+const { hostname, pathname } = window.location,
+    projectRegex = /^\/project\/([a-f0-9]+\-)+[a-f0-9]+\/?/;
 
-if(hostname === 'localhost') {
-  try{
-      baseUrl = pathname.match(/^\/project\/([a-f0-9]+\-)+[a-f0-9]+\/?/)[0];
-  } catch(e) {}
+if(hostname === 'localhost' && projectRegex.test(pathname)) {
+    baseUrl = pathname.match(projectRegex)[0];
 }
 
 export default (App) => {
