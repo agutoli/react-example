4bc2fac9d5f0db833df7928dfbfcebe99bf2f215
Ivan Torres <mexpolk@gmail.com>
2016-06-24 13:39:14 -0500

    add example for comments query

--- fragments/CommentFragmentLoaderContainer.js
+++ fragments/CommentFragmentLoaderContainer.js
@@ -5,10 +5,53 @@ import CommentFragmentContainer from './CommentFragmentContainer';
 
 export default Relay.createContainer(CommentFragmentLoader, {
     fragments: {
+        initialVariables: { contentId: null },
         app: () => Relay.QL`
-            fragment on Comment {
-                id,
-                ${CommentFragmentContainer.getFragment('comment')}
+            fragment on App {
+                comments(contentId: $contentId, first: 10, query: {
+                    projectId:"c293b805-03d6-4adb-8ccc-c54c104dc720",
+                    experienceId:"5b51be2f-3dfa-46cb-996d-8b0a87aadc76",
+                    filters:[
+                        {
+                            filterType:"IN",
+                            field:"SN_TYPE",
+                            values:[
+                                "ADVOCACY"
+                            ]
+                        },
+                        {
+                            filterType:"IN",
+                            field:"SN_MESSAGE_TYPE",
+                            values:[
+                                "203"
+                            ]
+                        },
+                        {
+                            field:"SOURCE_TYPE",
+                            filterType:"IN",
+                            values:[
+                                "ACCOUNT"
+                            ]
+                        }
+                    ],
+                    sorts:[
+                        {
+                            key:"snCTm",
+                            order:"ASC"
+                        }
+                    ],
+                    page:{
+                        page:0,
+                        size:3
+                    }
+                }) {
+                    edges {
+                        node {
+                            id,
+                            ${CommentFragmentContainer.getFragment('comment')}
+                        }
+                    }
+                }
             }
         `
     }
--- fragments/CommentFragmentLoaderRoute.js
+++ fragments/CommentFragmentLoaderRoute.js
@@ -1,66 +1,21 @@
 import Relay from 'react-relay';
 
-import ContentFragmentLoaderContainer from './CommentFragmentLoaderContainer.js';
-
 class CommentFragmentLoaderRoute extends Relay.Route {
     static routeName = 'CommentFragmentLoaderRoute';
 
     static queries = {
-        app: () => Relay.QL`
+        app: (Component, args) => Relay.QL`
             query {
                 app {
-                    comments(contentId: $contentId, first: 10, query: {
-                        projectId:"c293b805-03d6-4adb-8ccc-c54c104dc720",
-                        experienceId:"5b51be2f-3dfa-46cb-996d-8b0a87aadc76",
-                        filters:[
-                            {
-                                filterType:"IN",
-                                field:"SN_TYPE",
-                                values:[
-                                    "ADVOCACY"
-                                ]
-                            },
-                            {
-                                filterType:"IN",
-                                field:"SN_MESSAGE_TYPE",
-                                values:[
-                                    "203"
-                                ]
-                            },
-                            {
-                                field:"SOURCE_TYPE",
-                                filterType:"IN",
-                                values:[
-                                    "ACCOUNT"
-                                ]
-                            }
-                        ],
-                        sorts:[
-                            {
-                                key:"snCTm",
-                                order:"ASC"
-                            }
-                        ],
-                        page:{
-                            page:0,
-                            size:3
-                        }
-                    }) {
-                        edges {
-                            node {
-                                id,
-                                ${ContentFragmentLoaderContainer.getFragment('content')}
-                            }
-                        }
-                    }
+                    ${Component.getFragment('app', { contentId: args.contentId })}
                 }
             }
         `
-    };
-
-    static paramDefinitions: {
-        contentId: { required: true }
     }
+
+    static paramDefinitions = {
+        contentId: { required: true }
+    };
 }
 
 export default CommentFragmentLoaderRoute;
