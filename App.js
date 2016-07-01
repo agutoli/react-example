fc3e068913d64ac2d088763b1cdee2d2177ac079
Bruno Agutoli <bruno.agutoli@gmail.com>
2016-06-30 22:12:31 -0300

    added useConfig

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Relay from 'react-relay';
 import { render } from 'react-dom';
-import { Modal } from 'xb-common-lib';
+import { Modal, useConfig } from 'xb-common-lib';
 import AppRouter from './AppRouter';
 
 import './node_modules/xb-common-lib/dist/xbCommonLib.css';
 
 Relay.injectNetworkLayer(new Relay.DefaultNetworkLayer('/schema/data', {credentials: 'same-origin'}));
+
+// similiar with enviroment.js
+useConfig({
+  host: {
+    hostname: 'localhost',
+    port: '5000',
+    protocol: 'http:'
+  }
+});
 
 class App extends React.Component {
     render(){
